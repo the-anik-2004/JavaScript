3250cb37f61807008482215d4fe040f56082349a
Anik Pal <130134332+the-anik-2004@users.noreply.github.com>
2024-01-05 00:41:35 +0530

    01_variables is uploaded

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,31 @@
+const userId=28100122062;
+let userName="Anik Pal";
+var password="7267353";
+city="Kolkata";
+let state;
+
+//userId=28100122063;  //===>It is not allowed as it is CONSTANT variable
+
+userName="Rohan";
+password="123456";
+city="Bengaluru";
+
+
+// console.log(userId);
+console.table([userId,userName,password,city,state]);
+
+
+/*==================NOTES=================== */
+/*
+
+LINE-3
+    Prefer not to use var.
+    because of issue in block scope and functional scope\
+
+LINE-4
+    It is not a good prctice to have ..but  you can also define 
+    in that way.
+
+ LINE-5
+    As we don't define or assign any value ,so it is "undefined".  
+*/
