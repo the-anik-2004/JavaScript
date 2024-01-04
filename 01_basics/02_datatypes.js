0a7484c105913d78f515a279c55c76e5849b37bc
Anik Pal <130134332+the-anik-2004@users.noreply.github.com>
2024-01-05 00:33:15 +0530

    Datatypes are discussed

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";//===>treat all JS code newer as version
+
+let name="Anik Pal";
+var age=19;
+let isVerified=true;
+let container=null;
+let variable;
+
+console.table([typeof name,typeof age,typeof isVerified,typeof container,typeof variable]);
+/*
+
+*number =>2^53
+
+*bigint
+
+*string=>""
+
+*boolean=>true/flase
+
+*null=>It is standalone value.It simply means it is empty.
+and it is a object type.
+
+*undefined=>Value is not defined yet.It is undefined type.
+
+*symbol=>It defines uniqueness .
+
+*object
+*/
