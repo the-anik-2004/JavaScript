77280f8f56f414acfc81f35bc3d8df7213d63062
Anik Pal <130134332+the-anik-2004@users.noreply.github.com>
2024-01-05 01:44:58 +0530

    basic Operators

---
+++ 01_basics/04_basic.js
@@ -0,0 +1,25 @@
+// console.log(5+5);
+// console.log(5/5);
+// console.log(5-5);
+// console.log(5*5);
+// console.log(5**2);
+// console.log(5%3);
+
+// let str1="ANIK ";
+// let str2="PAL";
+// console.log(str1+str2);
+
+/**console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2); //==>If sting is the first element then string ADDITION will happen...all addtion will same.
+console.log(1+2+"2");//==>If number is the first element then number ADDITION will happen...after that string.
+ */
+
+
+/**===pre-increment  post-increment ===*/
+/**
+ * let i=0;
+// val=i++;  val<=0 -- i<=1
+// val=++i; val<=1 -- i<=1
+console.log(val);
+ */
