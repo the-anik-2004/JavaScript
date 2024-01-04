8a78f6fa00a6a2ce75cf354a4212683cb5b76fd6
Anik Pal <130134332+the-anik-2004@users.noreply.github.com>
2024-01-05 01:18:42 +0530

    conversion is discussed

---
+++ 01_basics/03_datatypeCoversion.js
@@ -0,0 +1,44 @@
+/**======Numbers =======*/
+// let score="81";  ==>Strings
+// let score="81anik";  ==>mixed strings->here coversion is succesfully happening but the value is NaN(Not a Number)
+// let score=null;  ==>converted value will be 0
+// let score=undefined;  ==>here coversion is succesfully happening but the value is NaN(Not a Number)
+// let score=true;  ==>here coversion is succesfully happening and the value is 1(true) 0(false)
+
+/*
+console.log("Datatype of 'score' :"+typeof(score));
+console.log("Value of 'score' :"+score);
+console.log();
+
+
+let intValue=Number(score);
+
+console.log("Datatype of 'intValue' :"+typeof(intValue));
+console.log("Value of 'intValue' :"+intValue);
+console.log();
+*/
+
+
+
+/**======Boolean values =======*/
+
+// let isValue=0;    ==>here coversion is succesfully happening and the value is 1(true) 0(false)
+// let isValue="gj"; ==>here coversion is succesfully happening and the value is "some text..."(true) ""(false)
+/*
+ let result=Boolean(isValue);
+
+console.log(typeof result);
+console.log(result);
+ */
+
+/**======Strings =======*/
+let num=12;
+let result=String(num);
+
+console.log(typeof result);
+console.log(result);
+
+
+
+
+
