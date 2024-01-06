d522230cd16f0617006b7ac6e976d9a98a9d386e
Anik Pal <130134332+the-anik-2004@users.noreply.github.com>
2024-01-06 22:33:16 +0530

    Array Basics

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -5,18 +5,39 @@
 // const arr=[1,2,4,5,7,8,5,4,"1nik",true,['hello',12]];==>You can store hetarogeneous data
 
 /**===========Array Basics========== */
-const myArr=[12,34,72,35];
-console.log(myArr);  //==>It is 0 based indexing
+const myArr=[1,2,3,4];
+// console.log(myArr);  //==>It is 0 based indexing
+// console.log(myArr.includes(12))
+// console.log(myArr.indexOf(34));
+
+// const newArr= myArr.join(); //==>It coverts to String
+// console.log(newArr);
+// console.log(typeof newArr);
+// console.log(newArr[1]);
+
+
+//=========Slice & Splice==========
+console.log("Orginal ",myArr);
+
+const newArr1=myArr.slice(1,3);//==>it does not Manupulates Original Array
+console.log("slice ",newArr1) ;
+console.log("Orginal ",myArr);
+
+const newArr2=myArr.splice(1,3);  //==>it Manupulates Original Array
+console.log("splice ",newArr2) ;
+console.log("Orginal ",myArr);
+
 
 const myFriends=new Array("Rana","sumon","Arnab","Debanjan","Snehadwip","Snighdha","Manojit")
 
-console.log(myFriends);
+// console.log(myFriends);
 
 /**===========Array Methods========== */
-myFriends.push("Me");
-console.log(myFriends);
-
-myFriends.pop("Me");
-console.log(myFriends);
+// myFriends.push("Me");
+// myFriends.push("You");
+// myFriends.pop();
+myFriends.unshift("Shubhashish"); //==>this unshift() METHODS add the elements at the first  ,for that reason rest  of the elements are shifted.If there are huge amount elements in that array,then this opration will be lenghty and heavy.So...TRY TO AVOID "unshift()".
+myFriends.shift();
+// console.log(myFriends);
 
 
