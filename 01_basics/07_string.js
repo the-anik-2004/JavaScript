ad6a543d9dacdace4c36f68c52f795e4938dbe22
Anik Pal <130134332+the-anik-2004@users.noreply.github.com>
2024-01-06 11:04:06 +0530

    Create 07_string.js

---
+++ 01_basics/07_string.js
@@ -0,0 +1,47 @@
+/**Standard way to declare and concat string */
+
+// let str1="Anik";
+// let num=123;
+// let str2="Namoskar";
+
+// console.log(`hello my name is ${str1} .my id is ${num}...${str2}`)
+
+
+/**========Another way to decalare a String:by creating Object======== */
+
+/*const youtubeCh= new String(" Code Space of Anik ");
+console.log(youtubeCh);
+console.log(youtubeCh[3]);
+console.log(youtubeCh.__proto__);
+console.log(youtubeCh.length);
+console.log(youtubeCh.toLocaleUpperCase()); 
+console.log(youtubeCh.charAt(12));
+console.log(youtubeCh.indexOf('S'));
+
+const newStr=youtubeCh.substring(0,4);
+console.log(newStr);
+
+const newStr2=youtubeCh.slice(-1,7)
+console.log(newStr2);
+
+console.log(youtubeCh.split(' '))
+
+console.log(youtubeCh);
+console.log(youtubeCh.trim())//A new string representing str stripped of whitespace from both its beginning and end.
+                                /**There is also trimStart()->trim the begging space
+                                 *               trimEnd()->trim the end space
+                                 */
+/*const email="codespace@google.com";
+console.log(email.replace('google','gmail'));
+console.log(email.includes("@"));
+ */
+
+
+//=====Intersting Methods=====
+
+// const a="a";
+// const b="b";
+// console.log(a<b);
+
+let str="ROHAN's house";
+console.log(str.search(`'`));
