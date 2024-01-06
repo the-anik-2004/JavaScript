df5a2ee0ff7928ac38c0f493167431c91b3025c7
Anik Pal <130134332+the-anik-2004@users.noreply.github.com>
2024-01-06 10:02:36 +0530

    Stack & Heap

--- 01_basics/06_dataTypes_summary.js
+++ 01_basics/06_dataTypes_summary.js
@@ -35,3 +35,33 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//Stack(Primitive),Heap(Non-Primitive)
+
+let a=10;
+let b=a;
+ a=20;
+
+ console.log(`a = ${a} : b = ${b}`);
+
+/**   STACK:-  
+ *      In a stack memory all the data type are changed in their copy
+     * not the real memory is affected.
+     
+ */
+let userOne={
+    name:"Ram",
+    userId:"ram@123"
+}
+
+let userTwo=userOne;
+userTwo.name="Sam";
+console.log(userOne.name)
+console.log(userTwo.name);
+
+/**  
+     * HEAP:-
+     *  In a heap memory all the data type are changed in their 
+     * real memory reference.so the original value is changed.
+     *  
+ */
