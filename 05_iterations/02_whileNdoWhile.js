767ce6c89960fe0bf62252a50f193bf9d41e3301
Anik Pal <130134332+the-anik-2004@users.noreply.github.com>
2024-01-12 23:46:16 +0530

    While and Do while loops

---
+++ 05_iterations/02_whileNdoWhile.js
@@ -0,0 +1,24 @@
+/**+++++++++++++++++While Loop+++++++++++++++++ */
+
+/*let index=0
+while(index<=10){
+//   console.log(`Value of index is ${index}`);
+  index+=2;
+}
+
+let myArr=['Flash','Batman','SuperMan'];
+let arrIn=0;
+while(arrIn<myArr.length){
+    console.log(`Value of index is ${myArr[arrIn]}`);
+    arrIn++;
+}
+ */
+
+
+/**+++++++++++++++++Do While Loop+++++++++++++++++ */
+
+let i=1;
+do{
+    console.log(i);
+    i++;
+}while(i<=10);
