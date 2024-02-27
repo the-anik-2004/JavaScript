6c6375100335fafd40a335faf83e874d8b57fd03
Anik Pal <130134332+the-anik-2004@users.noreply.github.com>
2024-02-27 10:55:05 +0530

    call method

---
+++ 10_OOP/call.js
@@ -0,0 +1,15 @@
+function setUserName(un){
+ this.userName=un;
+ console.log("USERNAME is SET");
+}
+
+function createUser(userName,email,password){
+  
+    // setUserName(userName)//actually this fn is not called it yet
+    setUserName.call(this,userName)//==>this and call are use here to pass the current context
+    this.email=email;
+    this.password=password;
+
+}
+const chai=new createUser("chai","chai@fb.com","12334");
+console.log(chai);
